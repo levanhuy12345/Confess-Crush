d1a9c076c00583550ec50b55542f6ff30f690c23
levanhuy12345 <92557392+levanhuy12345@users.noreply.github.com>
2021-12-15 10:21:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const CONFIG = {
     title: 'ANH KHÔNG BIẾT CẢM GIÁC NÀY CÓ TỪ LÚC NÀOO  🥰',
     desc: 'NHƯNG BÂY GIỜ ANH YÊU EM VÃI L*N LUÔN RỒIIIIII  . EM LÀM NGƯỜI YÊU ANH NHAAA:3',
     btnYes: ' DẠ VÂNG EM ĐỒNG Ý YÊU CÔNG CHÚA HUY :3',
-    btnNo: 'GHÉT MÀY NHẤT NHAA . ĐỒNG Ý CÁI ĐÉO HMMM',
+    btnNo: 'GHÉT MÀY NHẤT NHAAA . ĐỒNG Ý CÁI ĐÉO HMMM',
     question: 'SAO CÔNG CHÚA HAY DỖI LẠI YÊU CÔNG CHÚA HUYYY DỌOO <3',
     btnReply: 'Gửi cho CÔNG CHÚA HUY <3',
     reply: 'VÌ CÔNG CHÚA HUY DỄ THƯƠNG DỊU DÀNG VÀ YÊU EMMM <33333333',
